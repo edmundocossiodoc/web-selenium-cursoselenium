bef6ed4705ff895ed24738b8ef25e3124f658cc6
edmundocossiodoc <edmundo.cossio.doc@gmail.com>
2020-08-15 10:32:45 -0700

    first commit

---
+++ SeleniumAutomation/src/test/java/com/javabasics/test.java
@@ -0,0 +1,19 @@
+package com.javabasics;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   System.out.println("test");
+
+	}
+
+}
+
+/*
+ * Question 
+ * 	about maven
+ * 	variables de entorno
+ *  pom
+ *  si no quisiera usar maven, se podria?
+ * */
